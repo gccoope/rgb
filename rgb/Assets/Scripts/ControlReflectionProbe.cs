1b8cd7d829e99b9899447de7728262ad1c4a2e37
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2015-09-29 09:18:46 -0600

    Mirror stuff
    - created test_level
    - created a mirror prefab
    - created a ReflectionProbe prefab
    
    Still a lot of bugs, we'll talk about it in class

---
+++ rgb/Assets/Scripts/ControlReflectionProbe.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlReflectionProbe : MonoBehaviour {
+
+    // hmm... is it possible to just figure this out programmatically by looking at the gameObject.transform.rotation?
+    public enum Direction
+    {
+        X, Y, Z
+    }
+
+    [SerializeField] GameObject mirror;
+
+    private GameObject playerCam;
+    public float offset;
+    public Direction dir;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        // I don't understand why this doesn't work.
+
+        if(playerCam == null)playerCam = GameObject.FindGameObjectWithTag("player");
+        Vector3 probePos = gameObject.GetComponent<Transform>().position;
+
+        if (dir == Direction.X)
+        {
+            offset = mirror.transform.position.x - playerCam.transform.position.x;
+
+            probePos.x = mirror.transform.position.x + offset;
+            probePos.y = playerCam.transform.position.y;
+            probePos.z = playerCam.transform.position.z;
+        }
+
+        else if (dir == Direction.Y)
+        {
+            offset = mirror.transform.position.y - playerCam.transform.position.y;
+
+            probePos.x = playerCam.transform.position.x;
+            probePos.y = mirror.transform.position.y + offset;
+            probePos.z = playerCam.transform.position.z;
+        }
+
+        else if (dir == Direction.Z)
+        {
+            offset = mirror.transform.position.z - playerCam.transform.position.z;
+
+            probePos.x = playerCam.transform.position.x;
+            probePos.y = playerCam.transform.position.y;
+            probePos.z = mirror.transform.position.z + offset;
+        }
+
+        gameObject.GetComponent<Transform>().position = probePos;
+
+    }
+}
