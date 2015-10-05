571f107cb7b960a2451d8e9663aee9399a66e90c
Nathaniel Ellingson <nathaniel@administorsmbp2.uconnect.utah.edu>
2015-10-05 16:24:27 -0600

    Insect particle effect around lanterns
    - Once lanterns are lit, there is a random period of time (between 10
    and 120 seconds), after which a particle system will be activated.

--- rgb/Assets/Scripts/LanternBehavior.cs
+++ rgb/Assets/Scripts/LanternBehavior.cs
@@ -11,22 +11,35 @@ using System.Collections;
 
 [RequireComponent(typeof(Light))]
 [RequireComponent(typeof(SwitchBehavior))]
+[RequireComponent(typeof(ParticleSystem))]
 
 public class LanternBehavior : MonoBehaviour {
 
 	private bool lit; // is the lantern lit or unlit?
 	private Light lanternLight;
+	private ParticleSystem insectParticles; // "moth effect"
+	private float timeToStartPartSystem;
+	private float counter;
 
 	// Use this for initialization
 	void Start () {
 		lit = false; // start out unlit
 		lanternLight = gameObject.GetComponent<Light> ();
 		lanternLight.enabled = false;
+		insectParticles = GetComponent<ParticleSystem> ();
+		insectParticles.enableEmission = false;
+		timeToStartPartSystem = 10 + Random.value * 110; // start particle system some time between 10s and 120s
+		counter = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (lanternLight.enabled && !insectParticles.enableEmission) {
+			// start counter to initiate
+			Debug.Log ("Start countdown! t minus " + timeToStartPartSystem + " seconds");
+			counter += Time.deltaTime;
+			if(counter >= timeToStartPartSystem) insectParticles.enableEmission = true;
+		}
 	}
 
 	void OnTriggerEnter(Collider col)
